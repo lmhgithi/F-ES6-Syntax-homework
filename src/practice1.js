fcde074517bcc716e6f49c78d9a9c6e5e4743bf0
lmhgithi <lmhfox@foxmail.com>
2020-09-10 20:40:46 +0800

    [lmh] finish practice2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,16 @@
-const parseData
+const parseData = (input) => {
+    const keys=[];
+    let output=[];
+    input.column.map(item => (
+        keys.push(item.name)
+    ));
+    for(let data of input.data) {
+        let tmp={}
+        for(let i=0; i<data.length; i+=1){
+            tmp[keys[i]]=data[i];
+        }
+        output.push(tmp);
+    }
+    return output;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,12 @@
-const inject
+const inject = (items, sections) => {
+    let curProcessing = sections.length-1;
+    for (let i=items.length-1; i>=0; i-=1) {
+        if (curProcessing == -1) break;
+        if (sections[curProcessing].index == i) {
+            items=[...items.slice(0,i), sections[curProcessing].content, ...items.slice(i)]
+            curProcessing-=1;
+        }
+    }
+    return items;
+}
 export { inject };
