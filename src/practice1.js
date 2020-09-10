124559012af2401e058344634bd29658f63b066c
lmhgithi <lmhfox@foxmail.com>
2020-09-10 20:54:29 +0800

    [lmh] naming

--- src/practice1.js
+++ src/practice1.js
@@ -1,10 +1,10 @@
 const parseData = (input) => {
     let output=[];
-    const keys = input.column.map(item => item.name);
+    const columnKeys = input.column.map(item => item.name);
     for(let data of input.data) {
         let tmp={}
         for(let i=0; i<data.length; i+=1){
-            tmp[keys[i]]=data[i];
+            tmp[columnKeys[i]]=data[i];
         }
         output.push(tmp);
     }
--- src/practice2.js
+++ src/practice2.js
@@ -1,10 +1,10 @@
 const inject = (items, sections) => {
-    let curProcessing = sections.length-1;
+    let curSectionsPos = sections.length-1;
     for (let i=items.length-1; i>=0; i-=1) {
-        if (curProcessing == -1) break;
-        if (sections[curProcessing].index == i) {
-            items=[...items.slice(0,i), sections[curProcessing].content, ...items.slice(i)]
-            curProcessing-=1;
+        if (curSectionsPos == -1) break;
+        if (sections[curSectionsPos].index == i) {
+            items=[...items.slice(0,i), sections[curSectionsPos].content, ...items.slice(i)]
+            curSectionsPos-=1;
         }
     }
     return items;
