e07cb4b8f6c1c96f968842ff7f3dce27c4a4de46
kevindongzg <dongkevin94@gmail.com>
2020-09-22 00:04:35 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,8 +1,11 @@
 const parseData = ({data, column}) => {
+    // TODO 使用map做数组与数组的转化，不用声明output，查查map的使用
     let output=[];
+    // TODO columnKeys 没有必要
     const columnKeys = column.map(item => item.name);
     data.map(d => {
         const row={};
+        // TODO 可以尝试其他非 for循环 的遍历方式
         for(let i=0; i<d.length; i+=1){
             row[columnKeys[i]]=d[i];
         }
--- src/practice2.js
+++ src/practice2.js
@@ -1,3 +1,5 @@
+// TODO 逻辑纰漏，当sections内items顺序不是index顺序时会有问题
+// TODO 看一看 splice 相关用法
 const inject = (items, sections) => {
     let curSectionsPos = sections.length-1;
     for (let i=items.length-1; i>=0; i-=1) {
